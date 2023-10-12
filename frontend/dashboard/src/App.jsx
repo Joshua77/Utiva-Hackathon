8775bbb701dac040fcf89b8f0815a8e876cdb578
Joshua77 <jpakinola@gmail.com>
2023-10-12 17:49:54 +0100

    import Footer to Adminpage

--- frontend/dashboard/src/App.jsx
+++ frontend/dashboard/src/App.jsx
@@ -17,24 +17,23 @@
 // export default App;
 
 
-
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom'
 import SignupPage from "./components/signup/SignupForm"
 import LoginForm from "./components/login/LoginForm"
-import MainPage from './pages/adminboard/mainpage/mainpage';
+import MainPageLayout from './pages/adminboard/mainpage/LayoutMainpage';
+// import MainPage from './pages/adminboard/mainpage/mainpage';
 import LandingPage from "./LandingPage";
 
 function App(props) {
 return (
   <Router>
       <Routes>
-      <Route exact path="/" element={<LandingPage />} />
+      {/* <Route exact path="/" element={<LandingPage />} /> */}
+      <Route exact path="/" element={<MainPageLayout />} />
       <Route exact path="/login" element={<LoginForm />} />
       <Route exact path="/register" element={<SignupPage />} />
-      <Route exact path="/account" element={<MainPage />} />
+      {/* <Route exact path="/account" element={<MainPage />} /> */}
       </Routes>
   </Router>
 
---
+++ frontend/dashboard/src/pages/adminboard/mainpage/LayoutMainpage.jsx
@@ -0,0 +1,16 @@
+import MainPage from "./mainpage";
+import Footer from "../../../landingpagecomponents/footer/Footer";
+
+
+const MainPageLayout = ({children}) =>{
+    return (
+        <>
+          <MainPage />   
+          <Footer />
+        </>
+      )
+
+}
+
+
+export default MainPageLayout;

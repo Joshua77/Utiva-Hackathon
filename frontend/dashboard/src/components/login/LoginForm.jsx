475bc263e10e42d4ab60f05b251adbaf7af9115e
Joshua77 <jpakinola@gmail.com>
2023-10-12 17:31:22 +0100

    add Faruks code fix the css

--- frontend/dashboard/src/components/login/LoginForm.jsx
+++ frontend/dashboard/src/components/login/LoginForm.jsx
@@ -1,14 +1,40 @@
-import { useState } from "react";
-import "./LoginForm.css"
+// import { useState } from "react";
+// import "./LoginForm.css"
 
-function LoginForm() {
-  return (
-    <>
-      <div>
-        <h2>WELCOME TO LOGIN PAGE</h2>
+// function LoginForm() {
+//   return (
+//     <>
+//       <div>
+//         <h2>WELCOME TO LOGIN PAGE</h2>
         
-      </div>
-    </>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default LoginForm;
+
+
+
+import React from 'react';
+import './LoginForm.css'; // Import the CSS file
+import { Link } from 'react-router-dom';
+function LoginForm() {
+
+  return (
+    <div className="login-box">
+      <h2>Login | BINVA </h2>
+      <form>
+        <label>Email Address</label>
+        <input type="email" placeholder="username@gmail.com" />
+        <label>Password</label>
+        <input type="password" placeholder="*************" />
+        <input type="button" value="Submit" />
+      </form>
+      <p className="para-2">
+       <a href='#'>Forget Password </a>Not have an account? <Link to="/SignupForm">Sign Up</Link>
+      </p>
+    </div>
   );
 }
 
--- frontend/dashboard/src/components/signup/SignupForm.jsx
+++ frontend/dashboard/src/components/signup/SignupForm.jsx
@@ -1,15 +1,52 @@
-import { useState } from "react";
-import "./SignupForm.css";
+// import { useState } from "react";
+// import "./SignupForm.css";
+
+
+// function SignupPage() {
+//   return (
+//     <>
+//       <div>
+//         <h1>WELCOME TO SIGN UP PAGE</h1>
+        
+//       </div>
+//     </>
+//   );
+// }
+
+// export default SignupPage;
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SignupForm.css'; // Import the CSS file
 
 
 function SignupPage() {
+
   return (
-    <>
-      <div>
-        <h1>WELCOME TO SIGN UP PAGE</h1>
-        
-      </div>
-    </>
+    <div className="signup-box">
+      <h1>BINVA</h1>
+      <h4>Create an account for your Business </h4>
+      <form>
+        <label>Email Address</label>
+        <input type="email" placeholder="username@gmail.com" />
+        <label>Full Name</label>
+        <input type="text" placeholder="Binva Pro" />
+        <label>Password</label>
+        <input type="password" placeholder="**********" />
+        <label>Confirm Password</label>
+        <input type="password" placeholder="**********" />
+        <input type="button" value="Submit" />
+        <p className="para-2">
+          Already have an account?
+           <Link to="/LoginForm">Login</Link>
+           </p>
+      </form>
+      <p>
+        By clicking the Sign Up button, you agree to our <br />
+        <a href="#">Terms and Condition</a> and <a href="#">Policy Privacy</a>
+      </p>
+    </div>
   );
 }
 
